94a0de2f24853eefa2d41307d4f9767e33876cf4
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-09 14:28:42 -0600

    Add method to clean brewery data

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,5 @@
+import { fetchBreweryData } from './apiCalls'
+
 export const eventCleaner = (events) => {
   return events.map(event => {
     // console.log(event);
@@ -15,3 +17,8 @@ export const eventCleaner = (events) => {
     return {desc, groupName: event.group.name, time: event.local_time, date: event.local_date, ven, group, name: event.name, id: event.id }
   })
 }
+
+export const cleanBreweryData = async () => {
+  await fetchBreweryData()
+}
+cleanBreweryData()
