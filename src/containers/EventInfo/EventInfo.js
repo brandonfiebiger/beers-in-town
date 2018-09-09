bf1bfd9c8496cf4fa3a192fd9e17ad12ad7db5a6
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-09 15:15:29 -0600

    Fully fix router bug

--- src/containers/EventInfo/EventInfo.js
+++ src/containers/EventInfo/EventInfo.js
@@ -1,17 +1,11 @@
 import React from 'react';
-// import { withRouter } from 'react-router-dom';
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 
 export const EventInfo = (props) => {
-  console.log(props)
-  const handleRoute = () => {
-    props.history.push('/events')
-  }
 
   return(
     <div>
-      <button onClick={() => handleRoute()}>events</button>
       <h1>{props.cardsProps.name}</h1>
       <h2>By: {props.cardsProps.groupName}</h2>
       <h3>At: {props.cardsProps.ven.name}</h3>
