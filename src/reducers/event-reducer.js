86a411efbc2c473fb85b923b13da363c23cdf79c
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-09 17:45:48 -0600

    Add event fetch method to api calls, add dynamics for latitude and longitude

--- src/reducers/event-reducer.js
+++ src/reducers/event-reducer.js
@@ -1,4 +1,5 @@
 export const eventReducer = (state = [], action) => {
+  console.log(action)
   switch (action.type) {
     case 'EVENTS_FROM_LOCATION':
       return [...action.events];
--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,8 +1,14 @@
-import { breweryUrl } from './variables';
+import { apikey } from './variables';
 
-export const fetchBreweryData = async () => {
-  const response = await fetch(breweryUrl);
+
+export const fetchBreweryDataByLocation = async (latitude, longitude) => {
+  const response = await fetch(`https://api.meetup.com/find/venues?key=${apikey}&sign=true&photo-host=secure&text=brewery&lon=${longitude}&radius=100&lat=${latitude}&page=100`);
   const breweries = await response.json();
   return breweries;
 }
 
+export const fetchEventDataByLocation = async (latitude, longitude) => {
+  const response = await fetch(`https://api.meetup.com/find/upcoming_events?key=${apikey}&sign=true&photo-host=public&lon=${longitude}&page=50&text=craft beer, brewery&radius=50&lat=${latitude}`);
+  const breweries = await response.json();
+  return Promise.all(breweries.events);
+}
