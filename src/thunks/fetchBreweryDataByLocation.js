66196ea739b723950aa1c3be00a73fb9b116523f
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-09 21:00:03 -0600

    Add test for fetchBreweryData thunk for what it calls dispatch with

--- src/thunks/fetchBreweryDataByLocation.js
+++ src/thunks/fetchBreweryDataByLocation.js
@@ -6,6 +6,7 @@ export const fetchBreweryDataByLocation = (latitude, longitude) => {
     try {
       const response = await fetch(`https://api.meetup.com/find/venues?key=${apikey}&sign=true&photo-host=secure&text=brewery&lon=${longitude}&radius=100&lat=${latitude}&page=100`);
       const breweries = await response.json();
+      console.log(breweries)
       dispatch(populateBreweriesFromLocation(breweries))
     } catch (error) {
        throw new Error(error.message)
