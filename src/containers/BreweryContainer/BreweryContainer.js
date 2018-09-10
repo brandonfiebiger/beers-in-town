bfb28648b50582d7cace3ae45de9e14ffc6ed31c
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-09 21:29:41 -0600

    Add BreweryContainer and route to it from ContentRouter

---
+++ src/containers/BreweryContainer/BreweryContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+
+export class BreweryContainer extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return(
+      <section>
+        nothing
+      </section>
+    )
+  }
+}
