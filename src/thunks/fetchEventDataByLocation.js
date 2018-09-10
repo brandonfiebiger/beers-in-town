512a17b692e197dcdf3c7699163df57d6b56ee91
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-09 23:20:30 -0600

    Bump up the amount of breweries and events being displayed

--- src/thunks/fetchEventDataByLocation.js
+++ src/thunks/fetchEventDataByLocation.js
@@ -5,7 +5,7 @@ import { eventCleaner } from '../utils/helper';
 export const fetchEventDataByLocation = (latitude, longitude) => {
   return async (dispatch) => {
     try {
-      const response = await fetch(`https://api.meetup.com/find/upcoming_events?key=${apikey}&sign=true&photo-host=public&lon=${longitude}&page=100&text=craft beer, brewery&radius=100&lat=${latitude}`);
+      const response = await fetch(`https://api.meetup.com/find/upcoming_events?key=${apikey}&sign=true&photo-host=public&lon=${longitude}&page=2000&text=craft beer, brewery&radius=100&lat=${latitude}`);
       const events = await response.json();
       const cleanEvents = await eventCleaner(events.events);
       dispatch(populateEventsFromLocation(cleanEvents))
