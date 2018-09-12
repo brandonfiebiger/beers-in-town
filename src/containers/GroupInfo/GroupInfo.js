f815fb1e4d1b960405ebc753fa5225c9f1e1901c
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-11 19:04:15 -0600

    Create GroupInfo component

---
+++ src/containers/GroupInfo/GroupInfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+
+
+export const GroupInfo = (props) => {
+
+  return(
+    <section>
+      
+    </section>
+  )
+}
+
+export default GroupInfo;
