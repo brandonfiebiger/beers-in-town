281fe80154d97268ecb49548ff5dd99e8296ae74
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-17 21:14:17 -0600

    Add styles for EventInfo

--- src/containers/EventInfo/EventInfo.js
+++ src/containers/EventInfo/EventInfo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import PropTypes from 'prop-types';
+import './EventInfo.css';
 
 
 export const EventInfo = (props) => {
 
   return(
-    <div>
+    <div className="event-info">
       <h1>{props.eventProps.name}</h1>
       <h2>By: {props.eventProps.groupName}</h2>
       { props.eventProps.ven ? <h3>At: {props.eventProps.ven.name}</h3> : '' }
