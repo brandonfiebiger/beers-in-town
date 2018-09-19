a562285d0411324da170909cfa3a37d661129eed
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-19 00:30:55 -0600

    Delete unused variables from App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './App.css';
 import Search  from './containers/Search';
 import ContentRouter from './components/ContentRouter/ContentRouter';
-import { fetchEventDataByLocation } from '../src/thunks/fetchEventDataByLocation';
-import { fetchBreweryDataByLocation } from '../src/thunks/fetchBreweryDataByLocation';
 import { getLocation } from '../src/actions';
-import { fetchGroupDataByLocation } from '../src/thunks/fetchGroupDataByLocation';
-import { cleanGroupData } from '../src/utils/helper'
-
 
 export class App extends Component {
   
   componentDidMount() {
-    const { getGroups, getUserLocation } = this.props
+    const { getUserLocation } = this.props;
     navigator.geolocation.getCurrentPosition((location) => {
-      getUserLocation({latitude: location.coords.latitude, longitude: location.coords.longitude})
+      getUserLocation({latitude: location.coords.latitude, longitude: location.coords.longitude});
     });
   }
 
@@ -38,7 +33,7 @@ export class App extends Component {
 
 export const mapDispatchToProps = dispatch => ({
   getUserLocation: (location) => dispatch(getLocation(location)),
-})
+});
 
 export default withRouter(connect(null, mapDispatchToProps)(App));
 
