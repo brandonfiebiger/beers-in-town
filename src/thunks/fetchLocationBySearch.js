c51104123e7c14187ad6933ee4bdb2820c626fa8
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-20 10:13:15 -0600

    Change fetchGroupDataBySearch to fetchLocationBySearch

---
+++ src/thunks/fetchLocationBySearch.js
@@ -0,0 +1,23 @@
+import { apikey } from '../utils/variables';
+import  { getLocation, hasErrored } from '../actions';
+import { cleanGroupData } from '../utils/helper';
+import { fetchBreweryDataByLocation } from './fetchBreweryDataByLocation';
+import { fetchEventDataByLocation } from './fetchEventDataByLocation';
+import { fetchGroupDataByLocation } from './fetchGroupDataByLocation';
+
+
+export const fetchLocationBySearch = (city, state) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(`https://api.meetup.com/2/groups?key=${apikey}&sign=true&photo-host=public&country=us&city=${city}&state=${state}&text=brewery+craft+beer&page=100`);
+      const groups = await response.json();
+      const cleanedGroups = await cleanGroupData(groups.results);
+      dispatch(getLocation({latitude: groups.meta.lat, longitude: groups.meta.lon}));
+      dispatch(fetchBreweryDataByLocation(groups.meta.lat, groups.meta.lon))
+      dispatch(fetchEventDataByLocation(groups.meta.lat, groups.meta.lon))
+      dispatch(fetchGroupDataByLocation(groups.meta.lat, groups.meta.lon))
+    } catch (error) {
+      dispatch(hasErrored(true));
+    }
+  }
+}
