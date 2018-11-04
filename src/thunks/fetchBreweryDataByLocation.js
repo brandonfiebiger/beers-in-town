a8ca8acea3e26b5e5cbf00ffa917f4284a4be097
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-11-04 12:36:46 -0700

    Add heroku cors anywhere to all fetch calls

--- src/thunks/fetchBreweryDataByLocation.js
+++ src/thunks/fetchBreweryDataByLocation.js
@@ -4,7 +4,7 @@ import { populateBreweriesFromLocation, hasErrored } from '../actions/';
 export const fetchBreweryDataByLocation = (latitude, longitude) => {
   return async (dispatch) => {
     try {
-      const response = await fetch(`https://api.meetup.com/find/venues?key=${apikey}&sign=true&photo-host=secure&text=brewery&lon=${longitude}&radius=100&lat=${latitude}&page=200`);
+      const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/venues?key=${apikey}&sign=true&photo-host=secure&text=brewery&lon=${longitude}&radius=100&lat=${latitude}&page=200`);
       //add condition to check that response is ok check thunk lesson
       const breweries = await response.json();
       dispatch(populateBreweriesFromLocation(breweries))
--- src/thunks/fetchLocationBySearch.js
+++ src/thunks/fetchLocationBySearch.js
@@ -9,7 +9,7 @@ import { fetchGroupDataByLocation } from './fetchGroupDataByLocation';
 export const fetchLocationBySearch = (city, state) => {
   return async (dispatch) => {
     try {
-      const response = await fetch(`https://api.meetup.com/2/groups?key=${apikey}&sign=true&photo-host=public&country=us&city=${city}&state=${state}&text=brewery+craft+beer&page=100`);
+      const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.meetup.com/2/groups?key=${apikey}&sign=true&photo-host=public&country=us&city=${city}&state=${state}&text=brewery+craft+beer&page=100`);
       const groups = await response.json();
       const cleanedGroups = await cleanGroupData(groups.results);
       dispatch(noError(false));
