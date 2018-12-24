f95ba016a281855bb4abb6f135b5a13843691a06
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-12-23 18:56:41 -0700

    change out apiKey to enviroment variables

--- src/thunks/fetchBreweryDataByLocation.js
+++ src/thunks/fetchBreweryDataByLocation.js
@@ -4,8 +4,8 @@ import { populateBreweriesFromLocation, hasErrored } from '../actions/';
 export const fetchBreweryDataByLocation = (latitude, longitude) => {
   return async (dispatch) => {
     try {
-      const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/venues?key=${apikey}&sign=true&photo-host=secure&text=brewery&lon=${longitude}&radius=100&lat=${latitude}&page=200`);
-      //add condition to check that response is ok check thunk lesson
+      const url = `https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/venues?key=${process.env.REACT_APP_APIKEY}&sign=true&photo-host=secure&text=brewery&lon=${longitude}&radius=100&lat=${latitude}&page=200`;
+      const response = await fetch(url);
       const breweries = await response.json();
       dispatch(populateBreweriesFromLocation(breweries))
     } catch (error) {
--- src/thunks/fetchGroupDataByLocation.js
+++ src/thunks/fetchGroupDataByLocation.js
@@ -6,7 +6,7 @@ import { cleanGroupData } from '../utils/helper';
 export const fetchGroupDataByLocation = (latitude, longitude) => {
   return async (dispatch) => {
     try {
-      const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/groups?key=${apikey}&sign=true&photo-host=public&lon=${longitude}&text=craft beer&radius=100&lat=${latitude}&page=100`);
+      const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/groups?key=${process.env.REACT_APP_APIKEY}&sign=true&photo-host=public&lon=${longitude}&text=craft beer&radius=100&lat=${latitude}&page=100`);
       const groups = await response.json();
       const cleanedGroups = await cleanGroupData(groups);
       dispatch(populateGroupsFromLocation(cleanedGroups))
